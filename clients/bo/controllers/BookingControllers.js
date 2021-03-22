57f9586bc82acd0a01d340fdf66940aac8aaf0b3
kaiser-coder <miarintsoajohary@gmail.com>
2021-03-22 08:55:52 +0100

    creation des vues pour BookingControllers

--- clients/bo/controllers/BookingControllers.js
+++ clients/bo/controllers/BookingControllers.js
@@ -1,33 +1,54 @@
 /**
  * ========== Reservation ==========
  */
-const Booking = require('../models/Booking');
+const Booking = require("../models/Booking");
 
 /**
  * Affiche la liste des reservations
- * @param {object} req 
- * @param {object} res 
+ * @param {object} req
+ * @param {object} res
  */
 const list = (req, res) => {
-	res.send('Liste des reservations')
-}
+	const Booking = new Booking();
+
+	Booking.find()
+		.then((result) => {
+			res.render("main", { viewFile: "booking/list", bookings: result });
+		})
+		.catch((error) => {
+			throw error;
+		});
+};
 
 /**
  * Affiche le formulaire de reservation
- * @param {object} req 
- * @param {object} res 
+ * @param {object} req
+ * @param {object} res
  */
 const create = (req, res) => {
-	res.send('Formulaire de reservations')
-}
+	res.render("main", { viewFile: "booking/create" });
+};
 
 /**
  * Enregistre les informations des reservations
- * @param {object} req 
- * @param {object} res 
+ * @param {object} req
+ * @param {object} res
  */
 const store = (req, res) => {
-	res.send('Enregistrement des reservations')
-}
+	const booking = new booking(req.body);
 
-module.exports = {list, create, store};
+	booking
+		.save()
+		.then((result) => {
+			res.redirect("manage/list");
+		})
+		.catch((error) => {
+			throw error;
+		});
+};
+
+const view = (req, res) => {
+	res.render("main", { viewFile: "booking/view" });
+};
+
+module.exports = { list, create, store, view };
